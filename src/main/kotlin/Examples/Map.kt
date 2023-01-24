3bc605decd3bb0418836faa1707756be864afdf8
Sidharth K S <sidharthks60@gmail.com>
2023-01-24 11:17:55 +0530

    Commit2401023

--- src/main/kotlin/Examples/Map.kt
+++ src/main/kotlin/Examples/Map.kt
@@ -1,13 +1,16 @@
-    fun main() {
-        var student = mutableMapOf(1 to "Rohit", 2 to "Jitin", 3 to "Kumar")
-        println(student.keys)
+//Mutable map
+fun main() {
+    var student = mutableMapOf(1 to "Rohit", 2 to "Jitin", 3 to "Kumar")
+    println(student.keys)
 
-        for (i in student.keys) {
-            println("Key $i value is ${student[i]}")
-        }
-        println()
-        student.put(4, "Sidharth")
-        for (i in student.keys) {
-            println("Key $i value is ${student[i]}")
-        }
+    for (i in student.keys) {
+        println("Key $i value is ${student[i]}")
     }
+    println()
+
+    student.put(4, "Sidharth")
+
+    for (i in student.keys) {
+        println("Key $i value is ${student[i]}")
+    }
+}
